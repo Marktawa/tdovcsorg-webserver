d17790d9a6e554fe0456d058694da860f3e5c5ce
McTawa <markmunyaka@gmail.com>
2021-08-31 13:35:11 +0200

    tESt

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
-const path = require('path')
 const PORT = process.env.PORT || 5000
 require("dotenv").config();
 
